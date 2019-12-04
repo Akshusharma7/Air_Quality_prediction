c2309027532f830fbaa8dde17d1999393fbde006
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-04 09:12:43 +0530

    Create AQI_Script.py

---
+++ AQI_Script.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+def avg_data_2013():
+    temp_i = 0
+    average = []
+    for rows in pd.read_csv('Data/AQI/aqi2013.csv', chunksize=24):
+        add_var = 0
+        avg = 0.0
+        data = []
+        df = pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var = add_var+i
+            elif type(i) is str:
+                if i != 'NoData' and i != 'PwrFail' and i != '---' and i!= 'Invld':
+                    temp = float(i)
+                    add_var = add_var + temp
+        avg = add_var/24
+        temp_i = temp_i+1
+        
+        average.append(avg)
+    return average
+
+data1 = avg_data_2013()
+print(len(data1))
