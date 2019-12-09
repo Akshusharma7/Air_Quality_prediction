cc96aaca8cf1a1f82c9204f3c5994642b11df5fb
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-09 22:36:51 +0530

    Update Extract_combine.py

--- Extract_combine.py
+++ Extract_combine.py
@@ -1,7 +1,50 @@
-from Plot_AQI import avg_data_2013,avg_data_2014,avg_data_2015,avg_data_2016
+from AQI_Script import avg_data_2013,avg_data_2014,avg_data_2015,avg_data_2016
 import requests
 import sys
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
 import csv
+
+
+def met_data(month, year):
+    
+    file_html = open('Data/Html_Data/{}/{}.html'.format(year,month), 'rb')
+    plain_text = file_html.read()
+
+    tempD = []
+    finalD = []
+
+    soup = BeautifulSoup(plain_text, "lxml")
+    for table in soup.findAll('table', {'class': 'medias mensuales numspan'}):
+        for tbody in table:
+            for tr in tbody:
+                a = tr.get_text()
+                tempD.append(a)
+
+    rows = len(tempD) / 15
+
+    for times in range(round(rows)):
+        newtempD = []
+        for i in range(15):
+            newtempD.append(tempD[0])
+            tempD.pop(0)
+        finalD.append(newtempD)
+
+    length = len(finalD)
+
+    finalD.pop(length - 1)
+    finalD.pop(0)
+
+    for a in range(len(finalD)):
+        finalD[a].pop(6)
+        finalD[a].pop(13)
+        finalD[a].pop(12)
+        finalD[a].pop(11)
+        finalD[a].pop(10)
+        finalD[a].pop(9)
+        finalD[a].pop(0)
+
+    return finalD
+  
+  
