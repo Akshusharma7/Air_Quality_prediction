46fa11650265580793802f2518bdca52820bd1e9
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-21 20:13:29 +0530

    Update Html_script.py

--- Html_script.py
+++ Html_script.py
@@ -16,6 +16,7 @@ def retrieve_html():
                 url='http://en.tutiempo.net/climate/{}-{}/ws-421820.html'.format(month
                                                                           ,year)
             texts=requests.get(url)
+            
             text_utf=texts.text.encode('utf=8')
             
             if not os.path.exists("Data/Html_Data/{}".format(year)):
